e1f4527e54dd2f28d2777f5656e7c5cedeb39ef9
Alekay <alekaymusic@gmail.com>
2018-08-29 18:34:35 -0400

    get to rendered state

--- js/app.js
+++ js/app.js
@@ -1,38 +1,67 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+"use strict";
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+function randomNum(max, min) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+class Character{
+    constructor(horizontal, vertical, speed){
+        this.x = horizontal;
+        this.y = vertical;
+    }
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Enemy extends Character {
+    constructor(horizontal, vertical, speed) {
+        super(horizontal, vertical, speed);
+        this.speed = speed;
+        this.sprite = 'images/enemy-bug.png';
+    }
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x > 515) {
+            this.x = -125;
+            this.speed = randomNum(600, 150);
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
-
+let firstBug = new Enemy;
+let secondBug = new Enemy;
+let thirdBug = new Enemy;
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player extends Character {
+    constructor() {
+        super();
+        this.sprite = 'images/char-boy.png';
+        this.x = 202;
+        this.y = 386;
+    }
+    update() {
+        for(let i = 0; i < 3; i++) {
+            if (Math.abs(player.x - allEnemies[i].x) <= 81 && (player.y === allEnemies[i].y)) {
+                this.x = 202;
+                this.y = 386;
+            }
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [firstBug, secondBug, thirdBug];
 // Place the player object in a variable called player
-
-
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -46,11 +75,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-Resources.load([
-    'images/stone-block.png',
-    'images/water-block.png',
-    'images/grass-block.png',
-    'images/enemy-bug.png',
-    'images/char-boy.png',
-    'images/char-pink-girl.png'
-]);
